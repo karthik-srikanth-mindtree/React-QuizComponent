fe3b2d06fad3c2633f7d4f1124052f3c39b2724c
Karthik Srikanth <M1053736@mindtree.com>
2019-11-21 00:01:11 +0530

    reset quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,11 @@ class Quiz extends Component {
       quiz_position: 1
     };
   }
+  handleResetClick() {
+    this.setState({
+      quiz_position: 1
+    })
+  }
   showNextQuestion() {
     this.setState({
       quiz_position: this.state.quiz_position + 1
@@ -21,7 +26,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
         ) : (
           <QuizQuestion
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
